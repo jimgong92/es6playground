7182bbc2f2c0276723b2da9496850b921a7b19de
Jimmy Gong <gong.jim@gmail.com>
2015-06-19 16:54:45 -0700

    Reconfig gulp with babelify

--- gulpfile.js
+++ gulpfile.js
@@ -1,57 +1,39 @@
 var gulp = require('gulp');
-
-/**
- * UTILITIES
- */
+var sourcemaps = require('gulp-sourcemaps');
 var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
 var browserify = require('browserify');
 var watchify = require('watchify');
-var reactify = require('reactify');
-var plugins = require('gulp-load-plugins')();
-var path = {
-  HTML: 'src/index.html',
-  OUT: 'bundle.js',  
-  MINIFIED_OUT: 'bundle.min.js',
-  DEST: 'dist',
-  DEST_SRC: 'dist/src',
-  DEST_BUILD: 'dist/build',
-  ENTRY_POINT: './src/js/app.js'
+var babel = require('babelify');
+
+function compile(watch) {
+  var bundler = watchify(browserify('./src/index.js', { debug: true }).transform(babel));
+
+  function rebundle() {
+    bundler.bundle()
+      .on('error', function(err) { console.error(err); this.emit('end'); })
+      .pipe(source('build.js'))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({ loadMaps: true }))
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest('./build'));
+  }
+
+  if (watch) {
+    bundler.on('update', function() {
+      console.log('-> bundling...');
+      rebundle();
+    });
+  }
+
+  rebundle();
+}
+
+function watch() {
+  return compile(true);
 };
 
-/**
- * DEVELOPMENT TASKS
- */
-gulp.task('default', ['devReplace', 'watch']);
+gulp.task('build', function() { return compile(); });
+gulp.task('watch', function() { return watch(); });
 
-gulp.task('copy', function(){
-  gulp.src(path.HTML)
-    .pipe(gulp.dest(path.DEST));
-});
-gulp.task('devReplace', function(){
-  gulp.src(path.HTML)
-    .pipe(plugins.htmlReplace({
-      'js': 'src/' + path.OUT
-    }))
-    .pipe(gulp.dest(path.DEST));
-});
-gulp.task('watch', function(){
-  gulp.watch(path.HTML, ['copy']);
-
-  var watcher = watchify(browserify({
-    entries: [path.ENTRY_POINT],
-    transform: [reactify],
-    debug: true,
-    cache: {}, packageCache: {}, fullPaths: true
-  }));
-
-  return watcher.on('update', function(){
-    watcher.bundle()
-      .pipe(source(path.OUT))
-      .pipe(gulp.dest(path.DEST_SRC));
-      console.log('Updated');
-  })
-    .bundle()
-    .pipe(source(path.OUT))
-    .pipe(gulp.dest(path.DEST_SRC));
-
-});
+gulp.task('default', ['watch']);
