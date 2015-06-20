585cb3ef3fb47d0f9bb45b01e3b82144b86f4549
Jimmy Gong <gong.jim@gmail.com>
2015-06-19 20:02:34 -0700

    Begin play with default parameter values, spread operator

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,8 @@
-let logDefaults = (a=1) => { console.log(a) }
+let logDefaults = (a='Default Value', ...rest) => { 
+  console.log(a) 
+  if(rest.length > 0) logDefaults(rest)
+}
 logDefaults()
-logDefaults('Hello World!')
+logDefaults(undefined,'We','Are','Variadic','Args')
+logDefaults("Let's do some object destructuring")
+
